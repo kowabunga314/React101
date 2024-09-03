5b6773e1b006dbcab975962346c93493af36a8d8
kowabunga314 <dougfrost314@gmail.com>
2024-09-02 21:21:26 -0500

    update for array example

--- app/page.js
+++ app/page.js
@@ -2,21 +2,24 @@ import Image from "next/image";
 import { StatBox, Title } from "../components";
 
 export default function Home() {
+  const data = [
+    {label: 'Total Customers', value: '2500'},
+    {label: 'Total Products', value: '1000'},
+    {label: 'Total Categories', value: '100'},
+    {label: 'Average Revenue', value: '$2500'},
+    {label: 'Total Customers', value: '2500'},
+    {label: 'Total Products', value: '1000'},
+    {label: 'Total Categories', value: '100'},
+    {label: 'Average Revenue', value: '$2500'}
+  ]
+
   return (
     <div>
       <div>
         <Title className="ml-4" value="Dashboard" />
       </div>
       <div className="flex flex-wrap">
-        <StatBox label='Total Customers' value='2500' className='ml-10 mt-4' />
-        <StatBox label='Total Products' value='1000' className='ml-10 mt-4' />
-        <StatBox label='Total Categories' value='100' className='ml-10 mt-4' />
-        <StatBox label='Average Revenue' value='$2500' className='ml-10 mt-4' />
-
-        <StatBox label='Total Customers' value='2500' className='ml-10 mt-4' />
-        <StatBox label='Total Products' value='1000' className='ml-10 mt-4' />
-        <StatBox label='Total Categories' value='100' className='ml-10 mt-4' />
-        <StatBox label='Average Revenue' value='$2500' className='ml-10 mt-4' />
+        { data.map(el => <StatBox label={el.label} value={el.value} className='ml-10 mt-4' />) }
       </div>
     </div>
   );
---
+++ deprecated/app/page-basicFlexExample.js
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import { StatBox, Title } from "../components";
+
+export default function Home() {
+  return (
+    <div>
+      <div>
+        <Title className="ml-4" value="Dashboard" />
+      </div>
+      <div className="flex flex-wrap">
+        <StatBox label='Total Customers' value='2500' className='ml-10 mt-4' />
+        <StatBox label='Total Products' value='1000' className='ml-10 mt-4' />
+        <StatBox label='Total Categories' value='100' className='ml-10 mt-4' />
+        <StatBox label='Average Revenue' value='$2500' className='ml-10 mt-4' />
+
+        <StatBox label='Total Customers' value='2500' className='ml-10 mt-4' />
+        <StatBox label='Total Products' value='1000' className='ml-10 mt-4' />
+        <StatBox label='Total Categories' value='100' className='ml-10 mt-4' />
+        <StatBox label='Average Revenue' value='$2500' className='ml-10 mt-4' />
+      </div>
+    </div>
+  );
+}
