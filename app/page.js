4bf831b97bf2ae3e94bfda871bd858571d14ebe8
kowabunga314 <dougfrost314@gmail.com>
2024-09-05 22:21:45 -0500

    Callback/memo for calculator button.

--- app/page.js
+++ app/page.js
@@ -1,13 +1,14 @@
 "use client"
 
 import { Button, StatBox, Title, Input } from "../components";
-import { useState, useCallback } from "react";
+import { useState, useCallback, useMemo } from "react";
 
 export default function Home() {
   // Set page state variables
   const [numOne, setNumOne] = useState(0)
   const [numTwo, setNumTwo] = useState(0)
   const [total, setTotal] = useState(0)
+  const data = useMemo(() => {})
 
   // Set page state handlers
   const onNumOneChange = (event) => {setNumOne(event.target.value)}
@@ -27,7 +28,7 @@ export default function Home() {
         <input style={styles.input} onChange={onNumTwoChange} value={numTwo} />
         <br />
         <br />
-        <Button onClick={calculateTotal}>Calculate</Button>
+        <Button onClick={calculateTotal} data={data}>Calculate</Button>
       </div>
     </div>
   );
